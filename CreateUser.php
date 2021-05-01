72c68e157e68e473eea4ad50c31fe2b5ed3ed590
kmitch453 <79026320+kmitch453@users.noreply.github.com>
2021-05-01 18:07:29 -0500

    create query

--- CreateUser.php
+++ CreateUser.php
@@ -4,9 +4,16 @@ $mysqli = new mysqli("mysql.eecs.ku.edu", "kayleemitchell", "umahJ3es", "kayleem
 
 //check connection
 if ($mysqli->connect_errno) {
-  printf("Connect failed: %s\n", $mysqli->connect_error);
+  print("Connect failed: %s\n", $mysqli->connect_error);
   exit();
 }
+$uid = $_POST['user_id'];
+$query = "INSERT INTO Users VALUES ($uid)";
+if (mysqli.query($query)) {
+  print("Save succesful!");
+} else {
+  print("Save not successful");
+
 
 //close connection
 $mysqli->close();
