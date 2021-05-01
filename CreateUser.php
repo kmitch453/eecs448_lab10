527ddc1ee1f0022eb0007a33b60114c257ae658c
kmitch453 <79026320+kmitch453@users.noreply.github.com>
2021-05-01 17:14:15 -0500

    connection to database

--- CreateUser.php
+++ CreateUser.php
@@ -1,8 +1,13 @@
 
 <?php
+$mysqli = new mysqli("mysql.eecs.ku.edu", "kayleemitchell", "umahJ3es", "kayleemitchell");
 
+//check connection
+if ($mysqli->connect_errno) {
+  printf("Connect failed: %s\n", $mysqli->connect_error);
+  exit();
+}
 
-
-
-
+//close connection
+$mysqli->close();
 ?>
