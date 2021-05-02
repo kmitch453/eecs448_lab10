bf070ed8c136d56713d3e734d7a716fdff74fc2a
kmitch453 <79026320+kmitch453@users.noreply.github.com>
2021-05-01 23:16:16 -0500

    Update ViewUsers.php

--- ViewUsers.php
+++ ViewUsers.php
@@ -1,1 +1,23 @@
+<?php
+$mysqli = new mysqli("mysql.eecs.ku.edu", "kayleemitchell", "umahJ3es", "kayleemitchell");
 
+//check connection
+if ($mysqli->connect_error) {
+  die("Connect failed: %s\n" . $mysqli->connect_error);
+}
+
+$query = "SELECT * FROM Users";
+$result = $mysqli->query($query);
+
+if ($result->num_rows > 0) {
+  while($row = $result->fetch_assoc()) {
+    echo "UserID: " . $row["user_id"] . "<br>";
+  }
+  
+} else {
+  echo "No users created";
+}
+
+//close connection
+$mysqli->close();
+?>
