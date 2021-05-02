559a9d9c827688df8eca943c23d65b2a4cfc7139
kmitch453 <79026320+kmitch453@users.noreply.github.com>
2021-05-01 21:49:37 -0500

    null case

--- CreatePost.php
+++ CreatePost.php
@@ -1,1 +1,21 @@
+<?php
+$mysqli = new mysqli("mysql.eecs.ku.edu", "kayleemitchell", "umahJ3es", "kayleemitchell");
 
+//check connection
+if ($mysqli->connect_error) {
+  die("Connect failed: %s\n" . $mysqli->connect_error);
+}
+$uid = $_POST['user_id'];
+$np = $_POST['new_post'];
+if ($np!='') {
+  $query = "INSERT INTO Posts (author_id, content)  VALUES ('" . $uid . "', ' . $np . ')";
+}
+if ($mysqli->query($query)===TRUE) {
+  echo "Save succesful!";
+} else {
+  echo "Save not successful. Error: " . $query . "<br>" . $mysql->error;
+}
+
+//close connection
+$mysqli->close();
+?>
